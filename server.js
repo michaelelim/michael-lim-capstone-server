6a753bd88d42a1ecc5ff831de4f6cb3fab26d73a
Michael Lim <michaelelim@gmail.com>
2020-07-26 15:14:51 -0400

    Refactor for multirooms

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const axios = require('axios')
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
-let player1 = ""
-let player2 = 'JOIN NOW!'
+// let player1 = ""
+// let player2 = 'JOIN NOW!'
 
-let p1 = []
-let p2 = []
+// let p1 = []
+// let p2 = []
+let rooms = [];
 // let p1 = {name: "", id: "", score: 0, room: ""}
 // let p2 = {name: "", id: "", score: 0, room: ""}
 
@@ -31,171 +32,50 @@ io.on('connection', (socket) => {
   //   socket.join(room)
   // })
 
-  // Listen for name using algorithm
-  socket.on('userName', (name, clientId, room) => {
-  // socket.on('name1', (name, clientId) => {
-    if (name !== null && player1 === "" && player1 !== name) {
-      const p1Player = { name, clientId, room, score: 0}
-      p1.push(p1Player)
+  socket.on('userName', (name, clientId, code) => {
+    // name: 'mike'
+    // clientId: 'abc12356',
+    // room: 'ROOM
+    //
+    // if room doesn't exist, create new room
+    // if room already exists, check the players array within the existing room
+      // if the length is 2, do not allow entry
+      // if the length is 0 or 1, allow entry
 
-      player1 = name      
-
-      // p1.name = name
-      // p1.id = clientId
-      // p1.room = room
-      // p1.score = 0
-      console.log(p1)
-
-      socket.join(room)
-
-      io.emit('name1Broadcast', player1)
-      io.emit('p1Broadcast', p1)
-    } 
-    else if (name !== null && player2 === "JOIN NOW!" && player1 !== name) {
-      const p2Player = { name, clientId, room, score: 0}
-      p2.push(p2Player)
-      
-      player2 = name
-      
-      // p2.name = player2
-      // p2.id = clientId
-      // p1.score = 0
-      // p2.room = theRoom
-      console.log(p2)
-
-      socket.join(room)
-
-      io.emit('name1Broadcast', player1)
-      io.emit('name2Broadcast', player2)
-      io.emit('p1Broadcast', p1)
-      io.emit('p2Broadcast', p2)
+    const newPlayer = { name, score: 0, uuid: clientId };
+    if (!rooms.some(room => room.code === code)) {
+      const newRoom = { code, players: [newPlayer] };
+      rooms.push(newRoom);
+      socket.join(code);
+      io.emit('p1Broadcast', newPlayer);
+    } else {
+      const target = rooms.find(room => room.code === code);
+      if (target.players.length < 2) { target.players.push(newPlayer) } ;
+      socket.join(code);
+      io.emit('p2Broadcast', newPlayer);
     }
-  })
 
-  // listen for reiterate player names
-  socket.on('listPlayers', () => {
-    console.log("Reiterating player names: ", player1, player2)
-    io.emit('name1Broadcast', player1)
-    io.emit('name2Broadcast', player2)
-    io.emit('p1Broadcast', p1)
-    io.emit('p2Broadcast', p2)
-  })
-
-  // Listen for advance button
-  socket.on('advanceButton', (item, room) => {
-    if (item === "goToInstructions") {
-      io.emit('advanceToInstructions', item)
-      
-      axios
-        .get(`https://opentdb.com/api.php?amount=${numberOfQuestions}&type=multiple`)
-        .then(res => {sessionQuestions = res.data.results})
-        .catch(err => {console.log("Errors: ", err)})
-    }
-    if (item === "goToQuestionIntro") {io.emit('advanceToQuestionIntro', item)}
-    if (item === "goToQuestions") {io.emit('advanceToQuestions', item)}
+    console.log('rooms', rooms)
     
-    if (item === "goToWinner") {
-      if (!p2[0]) {
-        io.emit('advanceToWinner', p1[0].name, p1[0].score)
-      } else if (p1[0].score > p2[0].score) {
-        io.emit('advanceToWinner', p1[0].name, p1[0].score)
-      } else if (p1[0].score < p2[0].score) {
-        io.emit('advanceToWinner', p2[0].name, p2[0].score)
-      } else if (p1[0].score === p2[0].score) {
-        io.emit('advanceToWinner', "It's a Tie!", p1[0].score)
-      }
-    }
+    // [
+    //   {
+    //     code: 'ABC',
+    //     players: [
+    //       {
+    //         name: 'firstplayer',
+    //         score: 0,
+    //         uuid: 'xyz123'
+    //       },
+    //       {
+    //         name: 'secondplayer',
+    //         score: 0,
+    //         uuid: 'xyx987'
+    //       },
+    //     ]
+    //   }
+    // ]
   })
+});
 
-  // Listen for time to serve questions
-  const filteredQuestions = [];
-
-  // Fisher-Yates shuffle algorithm
-  const shuffle = (array) => {
-    let currentIndex = array.length;
-    let temporaryValue, randomIndex;
-  
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  };
-
-  // listen for request for questions
-  let questionsSent = false;
-
-  socket.on('sendQuestions', () => { 
-    if (questionsSent === false) {
-      console.log('Shuffling questions!')
-      questionsSent = true;
-      shuffle(sessionQuestions);
-      for (let i = 0; i < (numberOfQuestions); i++) {filteredQuestions.push(sessionQuestions[i])}
-    }
-    console.log("Filtered questions: ", filteredQuestions)
-    io.emit('filteredQuestions', filteredQuestions) //broadcast to all
-  })
-
-  // listen for signal for nextQuestion
-  socket.on('nextQuestion', () => {io.emit('nextQuestion')})
-
-  // listen for removeWrongAnswers
-  socket.on('removeWrongAnswer', (thisAnswer) => {
-    io.emit('removeWrongAnswer', thisAnswer)})
-
-  // listen for correct answers
-  socket.on('100Player', (id) => {
-    // console.log("id: ", id)
-    // console.log("id.clientId: ", id.clientId)
-    // console.log("p1: ", p1)
-    // console.log("p1[0].clientId: ", p1[0].clientId)
-    if (id.clientId == p1[0].clientId) {
-      p1[0].score += 100
-      console.log("p1: ", p1)
-      console.log("p2: ", p2)
-      console.log("p1.score: ", p1[0].score)
-      io.emit('100Player1', p1[0].name, p1[0].score)}
-    else if (id.clientId == p2[0].clientId) {
-      p2[0].score += 100
-      console.log("p1.score: ", p1[0].score)
-      io.emit('100Player2', p2[0].name, p2[0].score)}
-  })
-
-  // listen for incorrect answers
-  socket.on('minus75Player', (id) => {
-    if (id.clientId == p1[0].clientId) {
-      p1[0].score -= 75
-      console.log("p1.score: ", p1[0].score)
-      io.emit('minus75Player1', p1[0].name, p1[0].score)}
-    else if (p2[0].clientId.length !== 0 && id.clientId == p2[0].clientId) {
-      p2[0].score -= 75
-      console.log("p1.score: ", p1[0].score)
-      io.emit('minus75Player2', p2[0].name, p2[0].score)}
-  })
-
-  socket.on('resetRoom', (room) => {
-    console.log("p1 currently: ", p1)
-    console.log("p2 currently: ", p2)
-    console.log("Resetting room: ", room)
-
-    isRoom = (theRoom) => {theRoom.name === room}
-
-    const toRemoveP1 = p1.findIndex(isRoom)
-    const toRemoveP2 = p2.findIndex(isRoom)
-    
-    p1.splice(toRemoveP1, 1)       
-    p2.splice(toRemoveP2, 1)
-    player1 = ""
-    player2 = 'JOIN NOW!'
-
-    console.log("p1 afterwards: ", p1)
-    console.log("p2 afterwards: ", p2)
-  })
-})
-
-
-const PORT = 3009 || process.env.PORT
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const PORT = 3009 || process.env.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
